09c06d4a4b9590ec3e63ba8173846c99dcc3800c
JimboOneTwo <jimbo.one.two@gmail.com>
2021-06-23 10:30:12 +0300

    Release 1.1.1

--- pkg/container/container.go
+++ pkg/container/container.go
@@ -10,14 +10,30 @@ type Container struct {
 	services   map[string]interface{}
 }
 
-func (c *Container) Register(service string, handler func(c *Container) interface{}) {
+// NewContainer - the container constructor
+func NewContainer() *Container {
+	instance := &Container{
+		services:   make(map[string]interface{}),
+		registered: make(map[string]func(c *Container) interface{}),
+	}
+
+	instance.Register("container.Container", func(c *Container) interface{} {
+		return c
+	})
+
+	return instance
+}
+
+// Register - the function that registers services in container
+func (c *Container) Register(service string, resolver func(c *Container) interface{}) {
 	if c.registered[service] != nil {
 		log.Fatal(fmt.Sprintf("Service with name `%s` already registered in container", service))
 	}
 
-	c.registered[service] = handler
+	c.registered[service] = resolver
 }
 
+// Get - the function that extracts services from container by its service name
 func (c *Container) Get(service string) interface{} {
 	if c.services[service] != nil {
 		return c.services[service]
@@ -30,17 +46,4 @@ func (c *Container) Get(service string) interface{} {
 	c.services[service] = c.registered[service](c)
 
 	return c.services[service]
-}
-
-func NewContainer() *Container {
-	instance := &Container{
-		services:   make(map[string]interface{}),
-		registered: make(map[string]func(c *Container) interface{}),
-	}
-
-	instance.Register("container.Container", func(c *Container) interface{} {
-		return c
-	})
-
-	return instance
 }
